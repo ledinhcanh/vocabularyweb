c87ecd4e14763a1769533d981320febbfe5e0137
canhld <lecanh2810@gmail.com>
2026-03-06 15:51:19 +0700

    update

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 import { AuthService } from '../../services/auth.service';
@@ -8,15 +9,25 @@ import { CategoryService } from '../../services/category.service';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, ThemeToggleComponent],
+  imports: [RouterLink, ThemeToggleComponent, FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   authService = inject(AuthService);
   categoryService = inject(CategoryService);
+  private router = inject(Router);
+
+  searchKeyword = signal<string>('');
 
   constructor() {
     this.categoryService.getCategories().subscribe();
+  }
+
+  onSearch() {
+    const keyword = this.searchKeyword().trim();
+    if (keyword) {
+      this.router.navigate(['/search'], { queryParams: { q: keyword } });
+    }
   }
 }
